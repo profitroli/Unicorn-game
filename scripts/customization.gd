749d8ce78e502c3e70de6216682da53c0cbac4fe
profitroli <alisashestak2008@gmail.com>
2026-05-15 14:08:13 +0300

    5

--- scripts/customization.gd
+++ scripts/customization.gd
@@ -1,7 +1,25 @@
 extends Control
 
+@onready var panels = [
+	$TextureRect/ContentPanel/GrivaPanel, 
+	$TextureRect/ContentPanel/HvostPanel, 
+	$TextureRect/ContentPanel/RogPanel, 
+	$TextureRect/ContentPanel/TeloPanel
+]
+
 func _ready():
 	$TextureRect/MenuButton.pressed.connect(_on_back_pressed)
 
+	show_panel(0)
+	
+	$TextureRect/TabContainer/GrivaButton.pressed.connect(func(): show_panel(0))
+	$TextureRect/TabContainer/HvostButton.pressed.connect(func(): show_panel(1))
+	$TextureRect/TabContainer/RogButton.pressed.connect(func(): show_panel(2))
+	$TextureRect/TabContainer/TeloButton.pressed.connect(func(): show_panel(3))
+	
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
+
+func show_panel(index: int):
+	for i in range(panels.size()):
+		panels[i].visible = (i == index) 
--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -6,9 +6,23 @@ func _ready():
 	$TextureRect/CustomizationButton.pressed.connect(_on_customization_pressed)
 	$TextureRect/SettingsButton.pressed.connect(_on_settings_pressed)
 	$TextureRect/ExitButton.pressed.connect(_on_exit_pressed)
+	
+	$Control/TextureRect/Control/yes.pressed.connect(_on_confirm_exit)
+	$Control/TextureRect/Control/no.pressed.connect(_on_cancel_exit)
+	
+	$Control.visible = false
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/intro.tscn")
+
+func _on_exit_pressed():
+	$Control.visible = true
+
+func _on_confirm_exit():
+	get_tree().quit()
+
+func _on_cancel_exit():
+	$Control.visible = false
 
 func _on_save_pressed():
 	print("Кнопка сохранения (пока не работает)")
@@ -18,6 +32,3 @@ func _on_customization_pressed():
 
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://scenes/hub.tscn")
-
-func _on_exit_pressed():
-	get_tree().quit()
